1874e5a2d6d892a5dd91bf4e2126a94622823369
Jesus Illescas <jillesca@example.com>
2024-05-28 20:37:31 +0100

    starting to get xr live status

--- scripting_with_nso.py
+++ scripting_with_nso.py
@@ -2,18 +2,19 @@ from pprint import pprint as pp
 import ncs
 
 
-def see_devices_attributes():
+def see_devices_attributes() -> None:
     maapi = ncs.maapi.Maapi()
     maapi.start_user_session(user="admin", context="system", groups=[])
     transaction = maapi.start_write_trans()
 
     root = ncs.maagic.get_root(backend=transaction)
     for device in root.devices.device:
-        pp(dir(device))
+        if "core-rtr" in device.name:
+            pp(dir(device))
     maapi.close()
 
 
-def see_device_address():
+def see_device_address() -> None:
     maapi = ncs.maapi.Maapi()
     maapi.start_user_session(user="admin", context="system", groups=[])
     transaction = maapi.start_write_trans()
@@ -24,7 +25,7 @@ def see_device_address():
     maapi.close()
 
 
-def update_device_attribute_dry_run(device_name: str, hostname: str):
+def update_device_attribute_dry_run(device_name: str, hostname: str) -> dict:
     with ncs.maapi.single_write_trans(
         user="admin", context="system"
     ) as transaction:
@@ -39,12 +40,22 @@ def update_device_attribute_dry_run(device_name: str, hostname: str):
         print(f"{dry_run_result=}")
 
 
-def update_device_attribute(device_name: str, hostname: str):
+def update_device_attribute(device_name: str, hostname: str) -> None:
     with ncs.maapi.single_write_trans(
         user="admin", context="system"
     ) as transaction:
         root = ncs.maagic.get_root(backend=transaction)
         root.devices.device[device_name].config.hostname = hostname
+        transaction.apply()
+
+
+def show_xr_command(device_name: str, command: str):
+    with ncs.maapi.start_user_session(
+        user="admin", context="system", groups=[]
+    ) as transaction:
+        root = ncs.maagic.get_root(backend=transaction)
+        device = root.devices.device[device_name]
+        device.live_status.cisco_ios_xr_stats__exec(command)
         transaction.apply()
 
 
