01042353b971c4198d3c5581d75c111902d96ceb
Jesus Illescas <jillesca@example.com>
2024-05-30 20:07:57 +0100

    improving setting up script

--- build_workshop.sh
+++ build_workshop.sh
@@ -1,38 +1,41 @@
 #!/bin/bash 
 
-set -eu # Abort the script if a command returns with a non-zero exit code or if
-        # a variable name is dereferenced when the variable hasn't been set
 
-# set and save environment variables
+echo 'Setting environment variables'
 LAB_DIR=${HOME}/src
 NCS_RUN_DIR=${LAB_DIR}/workshop
 
-echo LAB_DIR=${HOME}/src >> /etc/environment
-echo NCS_RUN_DIR=${LAB_DIR}/workshop >> /etc/environment
-echo 'alias ll="ls -al"' >> $HOME/.bashrc
+echo 'alias ll="ls -al"' >> ${HOME}/.bashrc
+echo LAB_DIR=${HOME}/src >> ${HOME}/.bashrc
+echo NCS_RUN_DIR=${LAB_DIR}/workshop >> ${HOME}/.bashrc
 
-source /etc/environment
+echo 'Sourcing the bashrc'
+source ${HOME}/.bashrc
 
-# Create the lab directory
+echo 'Creating workshop directory'
 mkdir -p ${LAB_DIR}
 mkdir -p ${NCS_RUN_DIR}
 
-cd ${LAB_DIR}
 
-# start netsim devices
+echo 'Creating netsim devices'
 ncs-netsim --dir ${NCS_RUN_DIR}/netsim create-network $NCS_DIR/packages/neds/cisco-ios-cli-3.0 1 dist-rtr
 ncs-netsim --dir ${NCS_RUN_DIR}/netsim add-to-network $NCS_DIR/packages/neds/cisco-iosxr-cli-3.5 1 core-rtr
 
-# setup netsim
+echo 'Setting up netsim'
 ncs-setup --dest ${NCS_RUN_DIR} --netsim-dir ${NCS_RUN_DIR}/netsim
 
-# create the router package
+echo 'Creating the router package'
 ncs-make-package  --no-java --service-skeleton python-and-template --component-class router.Router --dest ${NCS_RUN_DIR}/packages/router router
 
-# start netsim devices and ncs
+echo 'start netsim devices'
 cd ${NCS_RUN_DIR}
 ncs-netsim start
+
+echo 'start ncs'
 ncs
 
-# sycn devices
+echo 'ncs status'
+ncs --status | grep -i status
+
+echo 'sycn devices'
 echo "devices sync-from" | ncs_cli -C -u admin
