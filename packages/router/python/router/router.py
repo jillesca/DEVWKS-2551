b05de16f000254d75d61aec84e7e5020c1d202b6
Jesus Illescas <jillesca@example.com>
2024-06-02 11:55:19 -0700

    simplyfing python logic

--- packages/router/python/router/router.py
+++ packages/router/python/router/router.py
@@ -8,26 +8,21 @@ class ServiceCallbacks(Service):
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
         self.log.info("Service create(service=", service._path, ")")
-        if service.sys.dns:
-            for server in service.sys.dns.server:
-                vars = ncs.template.Variables()
-                vars.add("DNS_ADDRESS", server.address)
-                template = ncs.template.Template(service)
-                template.apply("dns-template", vars)
 
-        if service.sys.syslog:
-            for server in service.sys.syslog.server:
-                vars = ncs.template.Variables()
-                vars.add("SYSLOG_ADDRESS", server.name)
-                template = ncs.template.Template(service)
-                template.apply("syslog-template", vars)
+        vars = ncs.template.Variables()
+        template = ncs.template.Template(service)
 
-        if service.sys.ntp:
-            for server in service.sys.ntp.server:
-                vars = ncs.template.Variables()
-                vars.add("NTP_ADDRESS", server.name)
-                template = ncs.template.Template(service)
-                template.apply("ntp-template", vars)
+        for server in service.sys.dns.server:
+            vars.add("DNS_ADDRESS", server.address)
+            template.apply("dns-template", vars)
+
+        for server in service.sys.syslog.server:
+            vars.add("SYSLOG_ADDRESS", server.name)
+            template.apply("syslog-template", vars)
+
+        for server in service.sys.ntp.server:
+            vars.add("NTP_ADDRESS", server.name)
+            template.apply("ntp-template", vars)
 
 
 class Router(ncs.application.Application):
